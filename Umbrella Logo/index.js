430efb77db01a1a3ff29f54bb975c5e9cb826db3
LordTejas <tejas5225@gmail.com>
2023-05-26 12:18:18 +0530

    Feature: Added Umbrella Color Options

--- Umbrella Logo/index.js
+++ Umbrella Logo/index.js
@@ -1,3 +1,10 @@
+const mainContainer = document.getElementById('main-container');
+const uploadBtn = document.getElementById('upload-btn');
+const umbrellaImage = document.getElementById('umbrella-img');
+const umbrellaLogo = document.getElementById('umbrella-logo');
+const loaderImage = document.getElementById('loader-img');
+
+
 function createColorElement(color) {
     let colorElement = document.createElement("div");
     colorElement.className = "color-element";
@@ -21,35 +28,85 @@ function addColorOptions() {
 function handleColorChange(e) {
     console.log(e.target.className);
 
-    const backgroundColors = {
-        "dodgerblue": "lightskyblue",
-        "salmon": "lightpink",
-        "yellow": "lightyellow"
-    }
+    const theme = {
+        "dodgerblue": {
+            color: "lightskyblue",
+            image: "./assets/Blue umbrella.png",
+        },
+        "salmon": {
+            color: "lightpink",
+            image: "./assets/Pink umbrella.png",
+        },
+        "yellow": {
+            color: "lightyellow",
+            image: "./assets/Yello umbrella.png",
+        }
+    };
+    
 
     if (e.target.className === "color-element") {
-        document.getElementById("main-container").style.backgroundColor = backgroundColors[e.target.style.backgroundColor];
-        document.getElementById("upload-btn").style.backgroundColor = e.target.style.backgroundColor;
+        mainContainer.style.backgroundColor = theme[e.target.style.backgroundColor].color;
+        uploadBtn.style.backgroundColor = e.target.style.backgroundColor;
+        umbrellaImage.src = theme[e.target.style.backgroundColor].image;
     }
 }
 
-function handleUploadFile() {
-
-    document.getElementById("upload-btn").addEventListener("click", function() {
-        document.getElementById("file-input").click();
-    });
+function changeUmbrella() {
     
-
-    const imgLogo = document.getElementById("img");
-    
-    // imgLogo.src = URL.document;
-
-    // Add Logo on our umbrella
-    // document.getElementById("img-show").appendChild(imgLogo);
 }
 
-function handleRemoveFile() {
+function handleSubmitFile(e) {
 
+    document.getElementById("loader-img").style.display = "block";
+    document.getElementById("umbrella-img").style.display = "none";
+    document.getElementById("umbrella-logo").style.display = "none"
+
+    setTimeout(() => {
+ 
+        const file = e.target.files[0];
+        
+        try {
+
+            if (!!file) {
+                document.getElementById("upload-btn-text").textContent = file.name.split("/").pop();
+                document.getElementById("upload-cancel-icon").style.display = "block";
+
+                const imgLogo = document.getElementById("umbrella-logo");
+                imgLogo.src = file.name;
+
+                
+                // Add Logo on our umbrella
+                imgLogo.style.display = "block";
+                
+            }
+        } catch(e) {
+            console.log(e);
+        } finally {
+            document.getElementById("loader-img").style.display = "none";
+            document.getElementById("umbrella-img").style.display = "block";
+        }
+        
+    }, 3000);
+}
+
+function handleRemoveFile(e) {
+    e.stopPropagation();
+    loaderImage.style.display = "block";
+    umbrellaImage.style.display = "none";
+    umbrellaLogo.style.display = "none"
+
+    setTimeout(() => {
+
+        try{
+            document.getElementById("upload-btn-text").textContent = "UPLOAD LOGO";
+        } catch(e) {
+            console.log(e);
+        } finally {
+            loaderImage.style.display = "none";
+            umbrellaImage.style.display = "block";
+        }
+        
+    }, 3000);
 }
 
 function init() {
@@ -60,7 +117,12 @@ function init() {
     // Add Event Listener to change Umbrella theme
     document.getElementById("color-options-div").addEventListener("click", handleColorChange);
 
-    document.getElementById("upload-btn").addEventListener('click', handleUploadFile);
+    uploadBtn.addEventListener("click", function() {
+        document.getElementById("file-input").click();
+    });
+
+    document.getElementById("file-input").addEventListener('change', handleSubmitFile);
+    document.getElementById("upload-cancel-icon").addEventListener('click', handleRemoveFile);
 
     // Simulate First Button Click by default
     document.getElementById("color-options-div").children[0].click();
