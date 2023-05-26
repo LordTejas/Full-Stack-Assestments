2ca484797d683475e1f931cf22df83f8658d218d
LordTejas <tejas5225@gmail.com>
2023-05-26 13:38:13 +0530

    Feature: Loader Screen added to the umbrella change event

--- Umbrella Logo/index.js
+++ Umbrella Logo/index.js
@@ -1,5 +1,6 @@
 const mainContainer = document.getElementById('main-container');
 const uploadBtn = document.getElementById('upload-btn');
+const uploadIcon = document.getElementById('upload-icon');
 const umbrellaImage = document.getElementById('umbrella-img');
 const umbrellaLogo = document.getElementById('umbrella-logo');
 const loaderImage = document.getElementById('loader-img');
@@ -25,6 +26,20 @@ function addColorOptions() {
 
 }
 
+function showLoader() {
+
+    document.getElementById("loader-img").style.display = "block";
+    document.getElementById("umbrella-img").style.display = "none";
+    const intitalState = document.getElementById("umbrella-logo").style.display;
+    document.getElementById("umbrella-logo").style.display = "none"
+
+    setTimeout(() => {
+        document.getElementById("loader-img").style.display = "none";
+        document.getElementById("umbrella-img").style.display = "block";
+        document.getElementById("umbrella-logo").style.display = intitalState;
+    }, 3000);
+}
+
 function handleColorChange(e) {
     console.log(e.target.className);
 
@@ -45,15 +60,13 @@ function handleColorChange(e) {
     
 
     if (e.target.className === "color-element") {
+        showLoader();
         mainContainer.style.backgroundColor = theme[e.target.style.backgroundColor].color;
         uploadBtn.style.backgroundColor = e.target.style.backgroundColor;
         umbrellaImage.src = theme[e.target.style.backgroundColor].image;
     }
 }
 
-function changeUmbrella() {
-    
-}
 
 function handleSubmitFile(e) {
 
@@ -62,18 +75,17 @@ function handleSubmitFile(e) {
     document.getElementById("umbrella-logo").style.display = "none"
 
     setTimeout(() => {
- 
-        const file = e.target.files[0];
+
+        const file = e.target.files[0];   
         
         try {
 
             if (!!file) {
-                document.getElementById("upload-btn-text").textContent = file.name.split("/").pop();
+                document.getElementById("upload-btn-text").textContent = file.name;
                 document.getElementById("upload-cancel-icon").style.display = "block";
 
                 const imgLogo = document.getElementById("umbrella-logo");
                 imgLogo.src = file.name;
-
                 
                 // Add Logo on our umbrella
                 imgLogo.style.display = "block";
@@ -117,7 +129,7 @@ function init() {
     // Add Event Listener to change Umbrella theme
     document.getElementById("color-options-div").addEventListener("click", handleColorChange);
 
-    uploadBtn.addEventListener("click", function() {
+    uploadIcon.addEventListener("click", function() {
         document.getElementById("file-input").click();
     });
 
